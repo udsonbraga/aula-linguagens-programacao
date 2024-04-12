bf3ebd9699cd2bc89e2c5af127f9a309b93a081e
udsonbraga <162235811+udsonbraga@users.noreply.github.com>
2024-04-12 15:53:39 -0400

    aula 11-04-2024
    ajuste na linha 4, para contar palavras sem o espaço.
    antes:
    numero_de_palavras = len(texto.split())
    
    depois:
    numero_de_palavras = texto.replace('  ', ' ')

--- python/conta-palavras.py
+++ python/conta-palavras.py
@@ -1,6 +1,6 @@
 
 texto = input("Digite um texto: ")
 
-numero_de_palavras = len(texto.split())
+numero_de_palavras = len(texto.replace(' ', ''))
 
 print(f"O número de palavras é: {numero_de_palavras}")
